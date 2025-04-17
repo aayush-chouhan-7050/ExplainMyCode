47a9c066baf9f3bcfb662c0060a1d25e9a8de381
Aayush Chouhan <aayushchouhan7050@gmail.com>
2025-04-17 20:03:54 +0530

    Fix : bugs fix

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use(
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: 'lax',
+      sameSite: 'none',
     },
   })
 );
